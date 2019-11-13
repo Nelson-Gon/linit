005cc6017ebe076fa3eb69df732b456f8df30a1c
Nelson-Gon <gonzabato@hotmail.com>
2019-11-13 12:08:47 +0800

    More is less

--- installer.sh
+++ installer.sh
@@ -7,50 +7,63 @@
 #!/bin/bash
 
 # Get updates
+update_system () {
 sudo apt update && sudo apt upgrade -y
+
+}
+add_ppas () {
 # add a few useful repositories
 # Boot-repair, things will go wrong and you'll need this at some point
-sudo add-apt-repository -y ppa:yannubuntu/boot-repair && sudo apt update
-sudo apt install -y  boot-repair
-# Propietary drivers
-sudo apt install -y ubuntu-restricted-extras
-# install vlc
-sudo apt install -y vlc
-# tweak your dekstop
-sudo apt install -y unity-tweak-tool
-sudo apt install -y gnome-tweak-tool
-# save some battery
-sudo apt install -y tlp tlp-rdw
-# start battery saver
-sudo tlp start
-# Install Caffeine and Caffeine Indicator
-sudo apt install -y caffeine
-# Get some cool themes
-# Comment out whatever you don't need
-# This will make Ubuntu look like a Mac
-# If you don't have the gnome shell, uncomment the next line
-# sudo apt install -y gnome-shell
-# select lightgdm when prompted
-# sudo apt update
-# add repo for macbuntu themes
-# noobslab, not so nooby
-sudo add-apt-repository -y ppa:noobslab/macbuntu
-sudo apt-update
-sudo apt install -y macbuntu-os-icons-v1804
-sudo apt install -y macbuntu-os-ithemes-v1804
+sudo add-apt-repository -y ppa:yannubuntu/boot-repair && yes | sudo apt update
+sudo add-apt-repository -y ppa:noobslab/macbuntu && yes | sudo apt update
+sudo add-apt-repository -y  'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' && yes | sudo apt update
+}
+install_tweakers () {
+
+sudo apt install -y  boot-repair ubuntu-restricted-extras unity-tweak-tool gnome-tweak-tool gnome-shell vlc tlp tlp-rdw caffeine plank macbuntu-os-icons-v1804 macbuntu-os-ithemes-v1804 macbuntu-os-plank-theme-v1804 vim
 # choose themes with tweak tool.
-# get the plank dock
-sudo apt install -y plank macbuntu-os-plank-theme-v1804
-# install pycharm
+}
+install_browsers (){
+sudo apt install -y firefox
+# Add Chrome
+pushd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm . *.deb
+popd
+}
+start_programs () {
+
+sudo tlp start
+sudo ufw enable
+}
+
+install_rpy () {
 sudo apt install -y snapd
-sudo snap install pycharm-community --classic
-# install r and rstudio
+yes | sudo snap install pycharm-community --classic
+# install R and RStudio(1.2.1335)
+
 sudo apt install -y r-base
 # install rstudio 1.2.1335
 sudo apt  install -y libx11-dev libxt-dev
 wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1335-amd64.deb
 yes | sudo dpkg -i rstudio-1.2.1335-amd64.deb
 rm rstudio*.deb
+# install pip3 and popular libraries(for ML enthusiasts)
+sudo apt install -y python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install jupyter pandas matplotlib sklearn scipy numpy requests seaborn
+}
+install_pdf_related () {
+# Not related to pdf but put here
+sudo apt install -y openssh openssl
+sudo apt install -y mupdf evince gimp okular texlive-latex-recommended
+# install pandoc
+# 2.7.3 at the time of writing
+wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
+yes | sudo dpkg -i pandoc*
+rm pandoc*.deb
+}
 # install python
 #sudo apt install -y build-essential checkinstall
 #sudo apt install -y libreadline-gplv2-dev libcursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdm-dev sudo apt install -y libc6-dev libbz2-dev libffi-dev zlib1g-dev
@@ -66,19 +79,8 @@ rm rstudio*.deb
 #sudo make altinstall
 # remove leftover  files
 #sudo rm /usr/src/Python-3.7.4.tgz
-
-
-# install pip3 and popular libraries(for ML enthusiasts)
-sudo apt install -y python3-pip
-sudo pip3 install --upgrade pip
-sudo pip3 install jupyter pandas matplotlib sklearn scipy numpy requests seaborn
-# install vim, yaaay
-sudo apt install  -y vim
 # set default to vim
 # sudo update-alternatives --config editor
-# choose vim
-# Enable Firewall
-sudo ufw enable
 # edit grub
 #vim /etc/default/grub
 # add image, acpi, quiet splash, timeout
@@ -87,37 +89,11 @@ sudo ufw enable
 # edit sudoers list
 # sudo /usr/sbin/visudo
 # proceed as you wish
-# install openssh
-sudo apt install -y openssh openssl
-# install mupdf/ okular
-sudo apt install -y mupdf
-# install tex
-sudo apt install -y texlive-latex-recommended
-# install pandoc
-# 2.7.3 at the time of writing
-wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
-yes | sudo dpkg -i pandoc*
-rm pandoc*.deb
-# install gparted gimp evince
-sudo apt install -y gimp
-sudo apt install -y evince
-# install firefox
-sudo apt install -y firefox
-# install chrome
-pushd ~/Downloads
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm . *.deb
-# install wine
-sudo add-apt-repository -y  'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-sudo apt update
-sudo apt-get install --install-recommends winehq-stable
-# install screenfetch and neofetch
-sudo apt install -y screenfetch neofetch
-# install rsync
-sudo apt install -y rsync
-# configure all packages manually
+misc_install () {
+sudo apt -y install --install-recommends winehq-stable
+sudo apt install -y screenfetch neofetch rsync
 sudo dpkg --configure --all
+}
 # Removes any old versions of docker
 remove_old_docker() {
 
@@ -132,6 +108,7 @@ install_docker () {
     yes | sudo systemctl start docker
     yes | sudo systemctl enable docker
 }
+
 remove_old_docker
 install_docker
 # Fancy exit
